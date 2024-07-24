c7af2e2dbb55843ffb50aaf72b1f357af5e56c46
arun <you@example.com>
2024-07-24 23:10:49 +0530

    commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
       <div className="grid text-center space-y-4">
-        <h1 className="text-4xl">Welcome to Next.js!</h1>
+        <h1 className="text-4xl">Welcome to Next.js! </h1>
         <Link className=" text-[#0000EE] underline" href={"/signup"}>
           {"Go to signup page!"}
         </Link>
